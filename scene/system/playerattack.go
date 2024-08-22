d033cb62f0c790524d63e07f018eea0619e41646
kharism <kharisma@kanosolution.com>
2024-08-22 21:07:56 +0700

    Some minor update
    - add some forgotten assets
    
    - make player have different sprite when attacking

--- scene/system/playerattack.go
+++ scene/system/playerattack.go
@@ -1,8 +1,11 @@
 package system
 
 import (
+	"time"
+
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
+	"github.com/kharism/grimoiregunner/scene/assets"
 	"github.com/kharism/grimoiregunner/scene/component"
 	"github.com/kharism/grimoiregunner/scene/system/attack"
 	"github.com/yohamta/donburi"
@@ -10,7 +13,8 @@ import (
 )
 
 type PlayerAttackSystem struct {
-	PlayerIndex *donburi.Entity
+	PlayerIndex     *donburi.Entity
+	returnToStandby time.Time
 }
 
 func NewPlayerAttackSystem(player *donburi.Entity) *PlayerAttackSystem {
@@ -24,6 +28,8 @@ func (s *PlayerAttackSystem) Update(ecs *ecs.ECS) {
 		// if time.Now().Sub(timerDelay) > 500*time.Millisecond {
 		playerId := ecs.World.Entry(*s.PlayerIndex)
 		gridPos := component.GridPos.Get(playerId)
+		component.Sprite.Set(playerId, &component.SpriteData{Image: assets.Player1Attack})
+		s.returnToStandby = time.Now().Add(500 * time.Millisecond)
 		attack.GenerateMagibullet(ecs, gridPos.Row, gridPos.Col+1, 15)
 		// timerDelay = time.Now()
 		// }
@@ -33,16 +39,24 @@ func (s *PlayerAttackSystem) Update(ecs *ecs.ECS) {
 		playerId := ecs.World.Entry(*s.PlayerIndex)
 		gridPos := component.GridPos.Get(playerId)
 		scrPos := component.ScreenPos.Get(playerId)
+		component.Sprite.Set(playerId, &component.SpriteData{Image: assets.Player1Attack})
+		s.returnToStandby = time.Now().Add(500 * time.Millisecond)
 		attack.NewLongSwordAttack(ecs, *scrPos, *gridPos)
 	}
 	if inpututil.IsKeyJustPressed(ebiten.KeyQ) {
 		playerId := ecs.World.Entry(*s.PlayerIndex)
 		gridPos := component.GridPos.Get(playerId)
+		component.Sprite.Set(playerId, &component.SpriteData{Image: assets.Player1Attack})
+		s.returnToStandby = time.Now().Add(500 * time.Millisecond)
 		attack.NewLigtningAttack(ecs, attack.LightnigAtkParam{
 			StartRow:  gridPos.Row,
 			StartCol:  gridPos.Col + 1,
 			Direction: 1,
 			Actor:     playerId,
 		})
+	}
+	if time.Now().After(s.returnToStandby) {
+		playerId := ecs.World.Entry(*s.PlayerIndex)
+		component.Sprite.Set(playerId, &component.SpriteData{Image: assets.Player1Stand})
 	}
 }
