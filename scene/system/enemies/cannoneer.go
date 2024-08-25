ebe7b83dab3a6851d793aa3c4ccb576f082c16bb
kharism <kharisma@kanosolution.com>
2024-08-26 05:10:44 +0700

    make cannoneer not use goroutine

--- scene/system/enemies/cannoneer.go
+++ scene/system/enemies/cannoneer.go
@@ -23,8 +23,14 @@ func NewCannoneer(ecs *ecs.ECS, col, row int) {
 	data := map[string]any{}
 	data[ALREADY_FIRED] = false
 	data[IS_MOVING] = false
+	data[LAST_FIRED] = nil
+	data[WARM_UP] = nil
+	data[CURRENT_STRATEGY] = ""
 	component.EnemyRoutine.Set(entry, &component.EnemyRoutineData{Routine: CannoneerRoutine, Memory: data})
 }
+
+var CURRENT_STRATEGY = "CurrMove"
+var WARM_UP = "WarmUp"
 
 // this enemy will move up-down and if it's on the same row as player
 // will attack
@@ -42,7 +48,50 @@ func CannoneerRoutine(ecs *ecs.ECS, entity *donburi.Entry) {
 	}
 	if v.Vx == 0 && v.Vy == 0 {
 		memory[IS_MOVING] = false
+		memory[CURRENT_STRATEGY] = ""
 	}
+	if memory[CURRENT_STRATEGY] == "" {
+		if playerGridPos.Row != entityGridPos.Row {
+			if moving, ok := memory[IS_MOVING].(bool); !ok || moving {
+				return
+			}
+			memory[CURRENT_STRATEGY] = "MOVE"
+			targetGridRowDirection := playerGridPos.Row - entityGridPos.Row
+			distance := float64(targetGridRowDirection) / math.Abs(float64(targetGridRowDirection))
+			targetRow := entityGridPos.Row + int(distance)
+			targetData := component.MoveTargetData{}
+			targetData.Tx, targetData.Ty = assets.GridCoord2Screen(targetRow, entityGridPos.Col)
+			pp, yy := assets.Coord2Grid(targetData.Tx, targetData.Ty)
+			fmt.Println(pp, yy)
+			component.TargetLocation.Set(entity, &targetData)
+			Vy := (targetData.Ty - entityScreenPos.Y)
+			Vx := 0.0
+			speedVector := csg.NewVector(Vx, Vy, 0)
+			speedVector = speedVector.Normalize().MultiplyScalar(1)
+			component.Speed.Set(entity, &component.SpeedData{Vx: 0, Vy: speedVector.Y})
+			memory[IS_MOVING] = true
+			// memory[CURRENT_STRATEGY] = ""
+		} else if fired, ok := memory[ALREADY_FIRED]; !(!ok || fired.(bool)) {
+
+			memory[CURRENT_STRATEGY] = "SHOOT"
+			memory[IS_MOVING] = false
+		}
+	}
+	if memory[CURRENT_STRATEGY] == "SHOOT" {
+		memory[WARM_UP] = time.Now().Add(750 * time.Millisecond)
+		memory[ALREADY_FIRED] = true
+		sprite.Image = assets.CannoneerAtk
+
+		memory[CURRENT_STRATEGY] = ""
+	}
+	if done, ok := memory[WARM_UP].(time.Time); ok && done.Before(time.Now()) {
+		fmt.Println("Enemy attack")
+		attack.GenerateMagibullet(ecs, entityGridPos.Row, entityGridPos.Col-1, -15)
+		memory[ALREADY_FIRED] = false
+		sprite.Image = assets.Cannoneer
+		memory[CURRENT_STRATEGY] = ""
+	}
+	/**
 	if playerGridPos.Row != entityGridPos.Row {
 		if moving, ok := memory[IS_MOVING].(bool); !ok || moving {
 			return
@@ -82,5 +131,5 @@ func CannoneerRoutine(ecs *ecs.ECS, entity *donburi.Entry) {
 			sprite.Image = assets.Cannoneer
 		}()
 
-	}
+	}*/
 }
